fc132414e4adc8c4e99a7954ece7a55abf97660e
fratardi <fratardi@students.42.fr>
2024-09-11 22:10:53 +0200

    errno stuff

--- ft_read.s
+++ ft_read.s
@@ -1,6 +1,13 @@
 section .text
     global ft_read ; (size_t `rax`)ft_read(int fd`rdi`, void*buf `rsi` , size_t count `rdx`)
+    extern ___errno_location;
 ft_read:
     mov rax, 0; Charge la valeur du syscall read  dans rax size_t read(int fd `rdi`, const void *buf`rsi`, size_t count)`rdx`;
-    syscall;    Appele le syscall charge dans rax 
+    syscall;    Appele le syscall charge dans rax
+    js reterror
     ret;
+reterror:
+    call ___errno_location   ; Obtenir l'adresse de errno
+    mov qword [rax], rax     ; Stocker la valeur de l'erreur (64 bits) dans errno
+    mov rax, -1              ; Retourner -1 pour indiquer l'erreur
+    ret
--- ft_write.s
+++ ft_write.s
@@ -1,7 +1,13 @@
 section .text
     global ft_write;   (size_t `rax`)write(int fd`rdi`, const void *buf`rsi`, size_t count`rdx`)
+    extern ___errno_location;
 ft_write:
     mov rax,  1;
     syscall;
+    js reterror;
     ret;
-;
+reterror:
+    call ___errno_location   ; Obtenir l'adresse de errno
+    mov qword [rax], rax     ; Stocker la valeur de l'erreur (64 bits) dans errno
+    mov rax, -1              ; Retourner -1 pour indiquer l'erreur
+    ret
